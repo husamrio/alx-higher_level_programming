f9b4febaf5386ab0eaacde88583a586dcd101f2a
husamrio <zulfiqrd@gmail.com>
2023-06-05 17:10:19 +0300

    0-run

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,5 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
+#include <stdlib.h>
 
 /**
  * struct listint_s - singly linked list
@@ -15,9 +16,9 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-size_t print_list(const listint_t *h);
-listint_t *add_node(listint_t **head, const int num);
-void free_list(listint_t *head);
-int check_cycle_list(listint_t *list);
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int num);
+void free_listint(listint_t *head);
+int check_cycle(listint_t *list);
 
 #endif /* LISTS_H */
