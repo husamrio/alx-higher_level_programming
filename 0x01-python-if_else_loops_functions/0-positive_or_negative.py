4838afd0099b3d11f3f49e689fabd1d9b5b49d33
husamrio <zulfiqrd@gmail.com>
2023-06-06 15:37:49 +0300

    + is better than -

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,9 +2,11 @@
 import random
 number = random.randint(-10, 10)
 # ********************
-if number < 0:
-    print("{:d} is negative".format(number))
-elif number > 0:
-    print("{:d} is positive".format(number))
+
+
+if number > 0:
+    print(f"{number:d} is positive")
+elif number < 0:
+    print(f"{number:d} is negative")
 else:
-    print("{:d} is zero")
+    print(f"{number:d} is zero")
