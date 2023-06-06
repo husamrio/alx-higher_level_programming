63d1d15d584f556d6545be014a8c339444d2ba61
husamrio <zulfiqrd@gmail.com>
2023-06-06 21:39:10 +0300

    Smile in the mirror

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+for ascii_value in range(122, 96, -2):
+        cs = f"{chr(ascii_value):s}{chr(ascii_value + ord('A') - ord('a') - 1):s}"
+        print("{:s}".format(cs), end="")
+
+# alternative method:
+# for i in range(ord('z'), ord('a')-1, -1):
+#    print('{:c}'.format(i) if i % 2 == 0 else chr(i-32), end='')
+# Another alternative method:
+# for i in range(0, 26):
+#    c = ord('z') - i
+#    if i % 2 == 1:
+#        c = (chr(c - ord('a') + ord('A')))
+#    else:
+#        c = chr(c)
+#    print("{}".format(c), end='')
