963bef2c4ae938ec97e99327033feac2b585a685
husamrio <zulfiqrd@gmail.com>
2023-06-06 19:37:41 +0300

    To uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
 def uppercase(str):
-    """Print a string in uppercase.
-
-    Args:
-        string (str): The string to print.
-
-    Returns:
-        None
-    """
     for j in str:
         if (ord(j) >= ord('a')) and (ord(j) <= ord('z')):
             j = chr(ord(j)-ord('a')+ord('A'))
