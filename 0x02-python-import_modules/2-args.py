dd6167be5cb7ac5384f94511fbca0f566b2d78c1
husamrio <zulfiqrd@gmail.com>
2023-06-08 15:38:31 +0300

    How to make a script dynamic!

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """Outputs the no. of lists of arguments."""
+    from sys import argv
+    userin = argv[1:]
+    size = len(userin)
+    argument_string = "arguments" if size != 1 else "argument"
+    punctuation = "." if size == 0 else ":"
+
+    print(f"{size} {argument_string}{punctuation}")
+
+    for idx, arg in enumerate(userin, start=1):
+        print(f"{idx}: {arg}")
