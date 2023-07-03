c11c8e2735b65c14f0b19e7b7770b0e2b8cd69c2
husamrio <zulfiqrd@gmail.com>
2023-07-04 00:43:29 +0300

    Simple rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Explains a Rectangle class
+Contains class Rectangle
+Empty class
+"""
+
+
+class Rectangle():
+    """
+    Explains empty class rectangle
+    """
+    pass
