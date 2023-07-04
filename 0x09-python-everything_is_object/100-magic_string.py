bda2b0e617379cf7ba8b0503e7985c98580b3105
husamrio <zulfiqrd@gmail.com>
2023-07-04 19:49:59 +0300

    pythonic

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,7 +1,4 @@
-def magic_string():
-    if not hasattr(magic_string, 'count'):
-        magic_string.count = 1
-    else:
-        magic_string.count += 1
-
-    return "BestSchool, " * (magic_string.count - 1) + "BestSchool"
+#!/usr/bin/python3
+def magic_string(s=[]):
+    s += ["BestSchool"]
+    return ", ".join(s)
