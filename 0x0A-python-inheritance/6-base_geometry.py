2f563a0d8bed68b2f4b12ad91bcc7fd8eaed4168
husamrio <zulfiqrd@gmail.com>
2023-07-10 18:05:31 +0300

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module 6-base_geometry:
+
+This module includes the `BaseGeometry` class.
+It has a public instance method named `area`.
+"""
+
+
+class BaseGeometry:
+    """
+    Methods:
+- `area(self)`: Calculates the area of the shape.
+    """
+    def area(self):
+        """Not implemented"""
+        raise Exception("area() is not implemented")
