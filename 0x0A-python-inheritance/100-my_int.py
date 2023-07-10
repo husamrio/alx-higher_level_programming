8806152cc35c94a6c214525512e4f7fed775915c
husamrio <zulfiqrd@gmail.com>
2023-07-11 00:12:56 +0300

    Can I?

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,31 +1,14 @@
 #!/usr/bin/python3
-"""
-Module 100-my_int contains a class called MyInt that inherits
-from the int class. The MyInt class is sneaky because it has
-inverted the == and != operators.
-"""
+"""Defines a class MyInt that inherits from int."""
 
 
 class MyInt(int):
-    """
-    The MyInt class has several methods, including an init method
-    that takes a num parameter, an eq method for checking equality,
-    and a ne method for checking inequality.
-    """
-    def __init__(self, num):
-        """initialize number"""
-        self.num = num
+    """Invert int operators == and !=."""
 
-    def __eq__(self, other):
-        """
-        The ne method returns
-        True if the two values being compared are not equal.
-        """
-        return self.num != other
+    def __eq__(self, value):
+        """Override == opeartor with != behavior."""
+        return self.real != value
 
-    def __ne__(self, other):
-        """
-        The eq method returns True if the
-        two values being compared are equal
-        """
-        return self.num == other
+    def __ne__(self, value):
+        """Override != operator with == behavior."""
+        return self.real == value
