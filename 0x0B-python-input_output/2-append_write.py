6037f15780b63ff92b92e45025a34bb5a6ca17c8
husamrio <zulfiqrd@gmail.com>
2023-07-11 15:53:45 +0300

    Append to a file

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,20 +1,13 @@
 #!/usr/bin/python3
-"""
-Module 2-read_lines contains a function that reads a specified
-number of lines from a file and outputs them to the screen.
-"""
+"""Module 2-read_lines contains a function that reads a specified
+number of lines from a file and outputs them to the screen.."""
 
 
-def read_lines(filename="", nb_lines=0):
+def append_write(filename="", text=""):
     """Reads a specified number of lines from a file and
     outputs them to the screen. If the specified number is
     less than 1 or greater than the total number of lines in the
-    file, the entire file is printed
+    file, the entire file is printed.
     """
-    with open(filename, mode="r", encoding="utf-8") as f:
-        if nb_lines <= 0:
-            print(f.read(), end="")
-        else:
-            while nb_lines:
-                print(f.readline(), end="")
-                nb_lines -= 1
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
