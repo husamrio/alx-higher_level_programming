8d35d178e33f87c2c7d022e257a62b9cbe27485c
root <root@Xrtls>
2023-08-18 02:25:23 +0300

    My privileges!

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,5 @@
---  All privileges of the MySQL users user_0d_1 and user_0d_2 on your server are listed by the script 
+--  All privileges of the MySQL users user_0d_1 and user_0d_2
+-- on your server are listed by the script 
 
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
