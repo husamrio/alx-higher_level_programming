10a5892ddc5f64ae8ee7d84804a1a28847bcc66d
husamrio <zulfiqrd@gmail.com>
2023-09-11 16:03:18 +0300

    loop to languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 
-
 const data = [
     'C is fun',
     'Python is cool',
     'JavaScript is amazing'
   ];
   
-  data.forEach(line => console.log(line));
+data.forEach(line => console.log(line));
