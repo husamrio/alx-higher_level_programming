3bb5e8cfe42fcc83ef2fd91f165b4b0210eed0f9
husamrio <zulfiqrd@gmail.com>
2023-09-11 15:38:15 +0300

    3-languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,3 +1,9 @@
 #!/usr/bin/node
 
-console.log('C is fun\nPython is cool\nJavascript is amazing');
+const data = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+
+data.forEach(line => console.log(line));
