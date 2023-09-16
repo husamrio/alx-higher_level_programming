3128f9b71215950ee597f76a0a9b006371afcc48
husamrio <zulfiqrd@gmail.com>
2023-09-16 22:45:21 +0300

    City relationship

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,36 +1,6 @@
-#!/usr/bin/python3
-"""
-parameters given to script: username, password, database
-use table relationship to access and print city and state
-*****
-"""
+-- Create the database hbtn_0e_100_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0e_100_usa;
+USE hbtn_0e_100_usa;
 
-from sqlalchemy.orm import sessionmaker
-from sys import argv
-from sqlalchemy import create_engine
-from relationship_state import Base, State
-from relationship_city import City
-
-
-if __name__ == "__main__":
-
-    # Create engine for database
-    user = argv[1]
-    passwd = argv[2]
-    db = argv[3]
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
-                           format(user, passwd, db), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    # utilise the table relationship to access and print city and state
-
-    rows = session.query(State).order_by(State.id).all()
-    for state in rows:
-
-        print("{}: {}".format(state.id, state.name))
-        for city in state.cities:
-            print("    {}: {}".format(city.id, city.name))
-
-    session.close()
+SELECT * FROM states;
+SELECT * FROM cities;
