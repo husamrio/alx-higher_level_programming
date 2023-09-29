618d33ba4088bd662672907f301a44f4a130638a
husamrio <zulfiqrd@gmail.com>
2023-09-29 12:13:25 +0300

    whats me status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,6 @@ fetch https://intranet.hbtn.io/status; display response
 import urllib.request
 
 if __name__ == "__main__":
-    
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
         print('Body response:')
